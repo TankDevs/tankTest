b54e39494f1191e9c0cff591d2f64497e1ce0298
周林华 <zhoulinhua@gwi.com.cn>
2017-07-12 10:36:10 +0800

    JavaDemo中增加异常测试

---
+++ JavaDemo/src/zlh/test/exception/MyException.java
@@ -0,0 +1,11 @@
+package zlh.test.exception;
+
+public class MyException extends RuntimeException {
+
+	private static final long serialVersionUID = -7787388997053821324L;
+
+	public MyException(String arg0) {
+		super(arg0);
+	}
+
+}
